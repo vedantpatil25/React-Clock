2327f1e0ba1c96b4d77072aec0b3c9cc32b86d9e
vedantpatil <vedantpatil2002@gmail.com>
2023-02-23 12:43:18 +0530

    this is the first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import "./index.css";
+let time = new Date();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+let currDate = new Date();
+let hrs =  currDate.getHours();
+let minutes = (currDate.getMinutes()).toLocaleString();
+
+
+
+let samay  = time.getHours();
+const cssStyle = {};
+console.log("Anand"); 
+let greeting = '';
+if(samay>=5 && samay< 12){
+  greeting = 'Good Morning';
+  cssStyle.color = "Green";
+
+  }
+else if (samay>=12 && samay<16){
+  greeting = 'Good Afternoon';
+  cssStyle.color = "Orange";
+
+}
+else if (samay>=16 && samay<20){
+  greeting = 'Good Evening';
+  cssStyle.color = "Grey";
+}
+else if(samay>=20 && samay<=24){
+  greeting = 'Good Night';
+}
+else{
+  greeting = 'It\'s too late ....Go to Sleep !';
+}
+ReactDOM.render(
+  <>
+  <div className = "container">
+    <div className = "small_container">
+      <h1>
+      Hello there, I am Vedant.
+    <br/>
+         <span style={cssStyle}>{greeting}!</span></h1>
+         <br />
+         This is {hrs} : {minutes}
+
+    </div>
+  </div>
+  </>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
